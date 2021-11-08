61fa02d37c4aa872554e7213a87120bd1a0cac62
emcrow <emcrow@sep.com>
2021-11-08 13:08:25 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['index.html', 'src/**/*.{vue,js,jsx,ts,tsx}'],
+  mode: 'jit',
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
